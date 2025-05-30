2bc0cdd5c23255baf3d7a5a1f98f0e91ff2fcf98
Daniel <daniel@example.com>
2025-05-30 13:32:56 +0200

    Añadí la clase main al repositorio de GitHub

--- src/clases/TrianguloRectangulo.java
+++ src/clases/TrianguloRectangulo.java
@@ -1,14 +1,14 @@
 package clases;
-
+/**
+ * @param a
+ * @param b
+ * @param c
+ */
 public class TrianguloRectangulo {
 	double a;
 	double b;
 	double c;
-	/**
-	 * @param a
-	 * @param b
-	 * @param c
-	 */
+
 	public TrianguloRectangulo() {
 		this.a = 1;
 		this.b = 1;
@@ -24,32 +24,50 @@ public class TrianguloRectangulo {
 		this.b = b;
 		this.c = hipotenusa();
 	}
+	/**
+	 * @return area
+	 */
 	public double area () {
 		double area;
 		area=this.a*this.b/2;
 		return area;
 	}
+	/**
+	 * @return this.c
+	 */
 	public double hipotenusa () {
 		this.c=Math.sqrt(this.a * this.a + this.b * this.b);
 		return this.c;
 	}
+	/**
+	 * @return perimetro
+	 */
 	public double perimetro() {
 		double perimetro;
 		perimetro=this.a+this.b+hipotenusa();
 		return perimetro;
 	}
+	/**
+	 * @return a
+	 */
 	public double getA() {
 		return a;
 	}
 	public void setA(double a) {
 		this.a = a;
 	}
+	/**
+	 * @return b
+	 */
 	public double getB() {
 		return b;
 	}
 	public void setB(double b) {
 		this.b = b;
 	}
+	/**
+	 * @return c
+	 */
 	public double getC() {
 		return c;
 	}
